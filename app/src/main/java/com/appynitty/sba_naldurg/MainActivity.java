63e6f5a923d4a60bfad470ab901f1f94b8629396
ayesha <appynitty.dev@gmail.com>
2021-02-04 17:36:24 +0530

    commit naldurg new version

--- app/src/main/java/com/appynitty/sba_naldurg/MainActivity.java
+++ app/src/main/java/com/appynitty/sba_naldurg/MainActivity.java
@@ -2,24 +2,36 @@ package com.appynitty.sba_naldurg;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.appynitty.swachbharatabhiyanlibrary.activity.SplashScreenActivity;
 import com.appynitty.swachbharatabhiyanlibrary.pojos.LanguagePojo;
 import com.appynitty.swachbharatabhiyanlibrary.utils.AUtils;
 import com.pixplicity.easyprefs.library.Prefs;
+import com.riaylibrary.utils.LocaleHelper;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    protected void attachBaseContext(Context base) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            super.attachBaseContext(LocaleHelper.onAttach(base, AUtils.LanguageConstants.MARATHI));
+        } else {
+            super.attachBaseContext(base);
+        }
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-Prefs.putInt(AUtils.VERSION_CODE,BuildConfig.VERSION_CODE);
+        Prefs.putInt(AUtils.VERSION_CODE,BuildConfig.VERSION_CODE);
         Prefs.putString(AUtils.APP_ID, "3039");
         initLanguageList();
 
